f2cd2df0a61ed919168e3f7ffde599ef5822cdae
Roman <roman@burningswell.com>
2011-08-27 20:53:48 +0200

    Initial import.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject geocode-clj "1.0.0-SNAPSHOT"
+  :description "A geocoding library for Clojure."
+  :dependencies [[clj-http "0.1.3"]
+                 [inflections "0.5.3-SNAPSHOT"]
+                 [org.clojure/clojure "1.2.1"]
+                 [org.clojure/data.json "0.1.1"]])
