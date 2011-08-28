4c596b85ab865eebbd95eb17e801eb03bd115d9e
Roman <roman@burningswell.com>
2011-08-28 17:42:11 +0200

    Added some address tests.

--- test/geocoder/test/address.clj
+++ test/geocoder/test/address.clj
@@ -1,7 +1,27 @@
 (ns geocoder.test.address
   (:use clojure.test
-        geocoder.address))
+        geocoder.address
+        geocoder.location))
+
+(def address
+  (make-address
+   :city "Berlin"
+   :country {:name "Germany" :iso-3166-1-alpha-2 "de"}
+   :location (make-location 52.54254 13.423033)
+   :street-name "Senefelderstraße"
+   :street-number "24"
+   :postal-code "10437"
+   :region "Berlin"))
 
 (deftest test-make-address
-  (let [address (make-address)]
-    ))
+  (let [country (:country address)]
+    (is (= "Germany" (:name country)))
+    (is (= "de" (:iso-3166-1-alpha-2 country))))
+  (is (= "Berlin" (:city address)))
+  (is (= "Senefelderstraße" (:street-name address)))
+  (is (= "24" (:street-number address)))
+  (is (= "10437" (:postal-code address)))
+  (is (= "Berlin" (:region address))))
+
+(deftest test-to-address
+  (is (= address (to-address address))))
