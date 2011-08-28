1e48bf1a6c5391c5b04a9d7e9cdb27e00d76eb2f
Roman <roman@burningswell.com>
2011-08-28 17:13:10 +0200

    Cleanup.

--- src/geocoder/core.clj
+++ src/geocoder/core.clj
@@ -1,34 +1,31 @@
 (ns geocoder.core
   (:require [clj-http.client :as client]
-            [geocoder.provider :as provider]
             geocoder.bing
             geocoder.google
             geocoder.yahoo)
   (:use [clojure.data.json :only (read-json)]
         [inflections.core :only (hyphenize-keys)]
-        geocoder.address))
+        geocoder.address
+        geocoder.provider))
 
 (def *provider*
   (geocoder.google.Provider.))
 
-(defn- request [request]
-  (-> (client/request request) :body read-json hyphenize-keys))
+(defn- fetch [request]
+  (->> (client/request request)
+       :body read-json hyphenize-keys
+       (results *provider*)
+       (map to-address)))
 
 (defn geocode
   "Geocode the address via the current *provider*."
   [address & options]
-  (->> (provider/geocode-request *provider* address options)
-       (request)
-       (provider/results *provider*)
-       (map to-address)))
+  (fetch (geocode-request *provider* address options)))
 
 (defn reverse-geocode
   "Reverse geocode the location via the current *provider*."
   [location & options]
-  (->> (provider/reverse-geocode-request *provider* location options)
-       (request)
-       (provider/results *provider*)
-       (map to-address)))
+  (fetch (reverse-geocode-request *provider* location options)))
 
 (defmacro with-provider
   "Evaluate body with provider bound to *provider*."
