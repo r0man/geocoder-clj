b4cee282d0482db409208047f821f1c3dbdb2be6
Roman <roman@burningswell.com>
2011-09-09 12:39:37 +0200

    Upgraded clj-http to v0.2.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject geocoder-clj "0.0.1-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
-  :dependencies [[clj-http "0.1.3"]
+  :dependencies [[clj-http "0.2.0"]
                  [inflections "0.5.3-SNAPSHOT"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/data.json "0.1.1"]])
