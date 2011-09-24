3a155821507b006788219b9cbb2cf110f6e8df75
Roman <roman@burningswell.com>
2011-09-24 11:18:12 +0200

    Removed snapshot versions.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject geocoder-clj "0.0.2"
   :description "A Clojure library for various geocoding services."
   :dependencies [[clj-http "0.2.0"]
-                 [inflections "0.6.0-SNAPSHOT"]
+                 [inflections "0.6.0"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]]
   :multi-deps {"1.2" [[clj-http "0.2.0"]
-                      [inflections "0.6.0-SNAPSHOT"]
+                      [inflections "0.6.0"]
                       [org.clojure/clojure "1.2.1"]
                       [org.clojure/data.json "0.1.1"]]})
