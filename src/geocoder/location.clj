61ca5920c7add6aff938bc0ef0f573c48476a8ef
Roman <roman@burningswell.com>
2011-09-29 23:37:50 +0200

    Added the distance-in-km fn.

--- src/geocoder/location.clj
+++ src/geocoder/location.clj
@@ -1,5 +1,7 @@
 (ns geocoder.location)
 
+(def earth-radius-km 6371.009)
+
 (defrecord Location [latitude longitude])
 
 (defn make-location [latitude longitude]
@@ -23,3 +25,14 @@
     (:longitude this))
   (to-str [this]
     (str (latitude this) "," (longitude this))))
+
+(defn distance-in-km
+  "Returns the distance between location-1 and location-2 in km."
+  [location-1 location-2 & [radius]]
+  (let [[lat1-r long1-r lat2-r long2-r]
+        (map #(Math/toRadians %)
+             [(latitude location-1) (longitude location-1)
+              (latitude location-2) (longitude location-2)])]
+    (* (or radius earth-radius-km)
+       (Math/acos (+ (* (Math/sin lat1-r) (Math/sin lat2-r))
+                     (* (Math/cos lat1-r) (Math/cos lat2-r) (Math/cos (- long1-r long2-r)))) ))) )
--- test/geocoder/test/location.clj
+++ test/geocoder/test/location.clj
@@ -19,3 +19,11 @@
     (is (= -2.7003 (:longitude location))))
   (is (= (make-location 43.4094 -2.7003)
          (make-location "43.4094" "-2.7003"))))
+
+(deftest test-distance-in-km
+  (let [berlin (make-location 52.54258 13.42299)
+        paris (make-location 48.856614 2.3522219)]
+    (is (= 0.0 (distance-in-km berlin berlin)))
+    (is (= 879.5734420795757 (distance-in-km berlin paris)))
+    (is (= (distance-in-km berlin paris)
+           (distance-in-km paris berlin)))))
