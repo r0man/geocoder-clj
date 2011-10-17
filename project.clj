b0509ad6bb5583c63984f5b19054f96e349a9f78
Roman <roman@burningswell.com>
2011-10-17 09:10:27 +0200

    Upgraded data.json and clj-http.

--- project.clj
+++ project.clj
@@ -1,11 +1,7 @@
 (defproject geocoder-clj "0.0.3-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
-  :dependencies [[clj-http "0.2.0"]
+  :dependencies [[clj-http "0.2.1"]
                  [inflections "0.6.0"]
                  [org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]
-                 [org.dspace.dependencies/dspace-geoip "1.2.3"]]
-  :multi-deps {"1.2" [[clj-http "0.2.0"]
-                      [inflections "0.6.0"]
-                      [org.clojure/clojure "1.2.1"]
-                      [org.clojure/data.json "0.1.1"]]})
+                 [org.clojure/data.json "0.1.2"]
+                 [org.dspace.dependencies/dspace-geoip "1.2.3"]])
