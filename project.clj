53d305af15196f927eef06326e59745cd5815755
Roman <roman@burningswell.com>
2011-10-28 13:17:59 +0200

    Release 0.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geocoder-clj "0.0.3-SNAPSHOT"
+(defproject geocoder-clj "0.0.3"
   :description "A Clojure library for various geocoding services."
   :dependencies [[clj-http "0.2.1"]
                  [inflections "0.6.3"]
