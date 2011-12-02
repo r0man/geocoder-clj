1c420414a8215ad98951d80b303ab6a6d8804ec1
Roman <roman@burningswell.com>
2011-12-02 19:07:52 +0100

    Switched to inflections 0.6.4-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject geocoder-clj "0.0.4-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
   :dependencies [[clj-http "0.2.1"]
-                 [inflections "0.6.3"]
+                 [inflections "0.6.4-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.dspace.dependencies/dspace-geoip "1.2.3"]]
   :multi-deps {"1.2.1" [[clj-http "0.2.1"]
-                        [inflections "0.6.3"]
+                        [inflections "0.6.4-SNAPSHOT"]
                         [org.clojure/clojure "1.2.1"]
                         [org.clojure/data.json "0.1.2"]
                         [org.dspace.dependencies/dspace-geoip "1.2.3"]]})
