e4b7219d620ed0396959e354759e79bf87df3c58
Roman <roman@burningswell.com>
2011-12-27 19:58:33 +0100

    Dropped Clojure 1.2.X support.

--- project.clj
+++ project.clj
@@ -4,9 +4,4 @@
                  [inflections "0.6.5-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [org.dspace.dependencies/dspace-geoip "1.2.3"]]
-  :multi-deps {"1.2.1" [[clj-http "0.2.6"]
-                        [inflections "0.6.5-SNAPSHOT"]
-                        [org.clojure/clojure "1.2.1"]
-                        [org.clojure/data.json "0.1.2"]
-                        [org.dspace.dependencies/dspace-geoip "1.2.3"]]})
+                 [org.dspace.dependencies/dspace-geoip "1.2.3"]])
