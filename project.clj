96cd376dfa7701f20329511e9a7aabd62aa0f1b0
Roman <roman@burningswell.com>
2012-03-13 21:35:36 +0100

    Get ready for Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject geocoder-clj "0.0.6-SNAPSHOT"
+(defproject geocoder-clj/geocoder-clj "0.0.6-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
   :dependencies [[clj-http "0.2.7"]
-                 [inflections "0.6.5-SNAPSHOT"]
+                 [inflections "0.7.0-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [org.dspace.dependencies/dspace-geoip "1.2.3"]])
+                 [org.dspace.dependencies/dspace-geoip "1.2.3"]]
+  :min-lein-version "2.0.0")
