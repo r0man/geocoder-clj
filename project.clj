f66328de33c4798b3260c265f006ef96925b8248
Roman <roman@burningswell.com>
2012-04-16 19:58:52 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure library for various geocoding services."
   :dependencies [[clj-http "0.2.7"]
                  [inflections "0.7.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.dspace.dependencies/dspace-geoip "1.2.3"]]
   :min-lein-version "2.0.0")
