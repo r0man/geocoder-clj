9f0c3dbb04d643a6cbcaa641943646cd1e591b06
Roman <roman@burningswell.com>
2012-04-18 22:41:18 +0200

    Indentation.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject geocoder-clj/geocoder-clj "0.0.6-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
+  :min-lein-version "2.0.0"
   :dependencies [[clj-http "0.2.7"]
                  [inflections "0.7.0-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [org.dspace.dependencies/dspace-geoip "1.2.3"]]
-  :min-lein-version "2.0.0")
+                 [org.dspace.dependencies/dspace-geoip "1.2.3"]])
