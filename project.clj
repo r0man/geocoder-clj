6213e811f025a2c031078a209379a3db2bffe871
Roman Scherer <roman.scherer@burningswell.com>
2012-08-12 20:59:04 +0200

    Switched to inflections 0.7.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure library for various geocoding services."
   :min-lein-version "2.0.0"
   :dependencies [[clj-http "0.4.0"]
-                 [inflections "0.7.0-SNAPSHOT"]
+                 [inflections "0.7.1-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.dspace.dependencies/dspace-geoip "1.2.3"]])
