b37e43b98b5c2ff77a562361fa37fd45f07309f1
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-09-13 20:39:59 -0500

    Switched to clj-http 0.5.5.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject geocoder-clj/geocoder-clj "0.0.6-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
   :min-lein-version "2.0.0"
-  :dependencies [[clj-http "0.4.0"]
+  :dependencies [[clj-http "0.5.5"]
                  [inflections "0.7.1-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
