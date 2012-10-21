c338829bab81eaf5288b89c529425b46f26367ef
Roman Scherer <roman.scherer@burningswell.com>
2012-10-21 15:13:08 +0200

    Started 0.0.9-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geocoder-clj/geocoder-clj "0.0.8"
+(defproject geocoder-clj/geocoder-clj "0.0.9-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
   :min-lein-version "2.0.0"
   :dependencies [[clj-http "0.5.6"]
