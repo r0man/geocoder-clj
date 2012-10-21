cad3870739a9b210fa4a41351d79d9f4796fd0c5
Roman Scherer <roman.scherer@burningswell.com>
2012-10-21 15:12:02 +0200

    Switched to clj-http 0.5.6.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject geocoder-clj/geocoder-clj "0.0.8-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
   :min-lein-version "2.0.0"
-  :dependencies [[clj-http "0.5.5"]
+  :dependencies [[clj-http "0.5.6"]
                  [inflections "0.7.3"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.2.0"]
