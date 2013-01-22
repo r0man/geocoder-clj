20245d60839f7ed598cc06f0bbe7c6fbae6d37d6
Roman Scherer <roman.scherer@burningswell.com>
2013-01-22 21:26:21 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject geocoder-clj/geocoder-clj "0.0.9-SNAPSHOT"
   :description "A Clojure library for various geocoding services."
   :min-lein-version "2.0.0"
-  :dependencies [[clj-http "0.5.6"]
+  :dependencies [[clj-http "0.6.3"]
                  [inflections "0.7.6"]
                  [org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.0"]
+                 [org.clojure/data.json "0.2.1"]
                  [org.dspace.dependencies/dspace-geoip "1.2.3"]])
