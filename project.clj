7f33a08dc50da149273bbd33e0e23244e9b90283
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:16:17 +0100

    Added license and url.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject geocoder-clj/geocoder-clj "0.1.1"
   :description "A Clojure library for various geocoding services."
+  :url "https://github.com/r0man/geocoder-clj"
   :min-lein-version "2.0.0"
+  :author "Roman Scherer"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "0.6.4"]
                  [inflections "0.8.0"]
                  [org.clojure/clojure "1.5.0"]
