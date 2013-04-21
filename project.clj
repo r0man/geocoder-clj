88a004d34c79c5e9a6640cb92d26f7c1e729df90
Roman Scherer <roman.scherer@burningswell.com>
2013-04-21 20:01:55 +0200

    Use geo-clj 0.2.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geocoder-clj "0.2.0-SNAPSHOT"
+(defproject geocoder-clj "0.2.0"
   :description "A Clojure library for various geocoding services."
   :url "https://github.com/r0man/geocoder-clj"
   :min-lein-version "2.0.0"
@@ -6,7 +6,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "0.7.2"]
-                 [geo-clj "0.2.3-SNAPSHOT"]
+                 [geo-clj "0.2.3"]
                  [inflections "0.8.1"]
                  [org.clojure/clojure "1.5.1"]
                  [org.dspace.dependencies/dspace-geoip "1.2.3"]])
