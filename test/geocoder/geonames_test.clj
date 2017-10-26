5fde2ba56b5304e07f7b58afb1e2bfa2451d1f4a
Stuart Hinson <stuart.hinson@gmail.com>
2017-10-26 11:46:41 -0400

    Fix geonames test

--- test/geocoder/geonames_test.clj
+++ test/geocoder/geonames_test.clj
@@ -1,6 +1,7 @@
 (ns geocoder.geonames-test
   (:require [clojure.test :refer :all]
             [geo.core :refer [point point-x point-y]]
+            [geocoder.utils :refer [approx=]]
             [geocoder.geonames :refer :all]))
 
 (def test-key "geonamesclj")
@@ -40,15 +41,15 @@
   (is (empty? (geocode-address "xxxxxxxxxxxxxxxxxxxxx" :api-key test-key)))
   (let [address (first (geocode-address "Berlin" :api-key test-key))]
     (is (nil? (street-name address)))
-    (is (= "10117" (postal-code address)))
+    (is (= "10587" (postal-code address)))
     (is (= "Berlin" (city address)))
     (is (= "Berlin" (region address)))
     (let [country (country address)]
       (is (= "de" (:iso-3166-1-alpha-2 country)))
       (is (nil? (:name country))))
     (let [location (location address)]
-      (is (= 52.5161166666667 (point-y location)))
-      (is (= 13.38735 (point-x location))))))
+      (is (approx= 52.5161166666667 (point-y location)))
+      (is (approx= 13.319519141740923 (point-x location))))))
 
 (deftest test-geocode-location
   (is (empty? (geocode-location (point 4326 0 0) :api-key test-key)))
@@ -61,8 +62,8 @@
       (is (= "de" (:iso-3166-1-alpha-2 country)))
       (is (nil? (:name country))))
     (let [location (location address)]
-      (is (= 52.5161166666667 (point-y location)))
-      (is (= 13.38735 (point-x location)))))
+      (is (approx= 52.5161166666667 (point-y location)))
+      (is (approx= 13.38735 (point-x location)))))
   (is (= (geocode-location (point 4326 13.42299 52.54258) :api-key test-key)
          (geocode-location "52.54258,13.42299" :api-key test-key)
          (geocode-location {:latitude 52.54258 :longitude 13.42299} :api-key test-key)
